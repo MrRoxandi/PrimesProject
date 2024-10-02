3a31a69392b3cabf472579b76f43e9cb89f40313
Roxandi <kosty2004askzvwe@gmail.com>
2024-10-02 08:23:23 +0700

    fixes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 #include <random>
-#include "src/sieve.hpp"
+#include "src/integer.hpp"
 
 int main()
 {
-    const uint64_t size = 25;
-    std::mt19937_64 generator(std::random_device{}());
-    std::uniform_int_distribution<unsigned int> dist(0, 1);
-    clampedBits left(size, 0), right(size, 1);
-    for (uint64_t idx = 0; idx < size; ++idx)
-    {
-        left.set(idx, dist(generator));
-        right.set(idx, dist(generator));
-    }
-    std::cout << "Left:\t" << left << '\n';
-    std::cout << "Right:\t" << right << '\n';
-    std::cout << "Result:\t" << (left ^ right) << '\n';
+    integer number(clampedBits("1010101010101010101010101010101010101010101"));
+    integer number2(clampedBits("01010101010101010101010101010101010101010101"));
+    std::cout << number2 << '\n';
 }
