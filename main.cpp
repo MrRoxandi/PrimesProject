c5276c8d5de6c4d47c5945eba7cb4a4d2186a0f1
Roxandi <kosty2004askzvwe@gmail.com>
2024-10-02 18:23:25 +0700

    HUGE improvement

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+<<<<<<< HEAD
 #include "src/integer.hpp"
 
 int main()
@@ -9,4 +10,16 @@ int main()
     std::cout << "Left:\t" << left << '\n';
     std::cout << "Right:\t" << right << '\n';
     std::cout << std::boolalpha << (left < right) << '\n';
+=======
+#include "src/sieve.hpp"
+#include <string>
+int main()
+{
+    clampedBits left = "101111101101110111101111101101110111101111101101110111101010101010110101";
+    std::cout << "Left:\t" << left << '\n';
+    left >>= 67;
+    std::cout << "Left:\t" << left << '\n';
+    // "101111101101110111101111101101110111101111101101110111101010101010110101"
+    // "000000000000000000000000000000000000000000000000000000000000000000010101"
+>>>>>>> 03e042d (HUGE improvement)
 }
