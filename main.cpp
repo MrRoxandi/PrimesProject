66ed316d9088faacee1e0d29c6a2a028057ec92e
Roxandi <kosty2004askzvwe@gmail.com>
2024-10-03 19:48:44 +0700

    update 03.10.2024

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
+#include "src/clampedBits.hpp"
+#include <format>
 #include <iostream>
 #include <random>
-#include "src/clampedBits.hpp"
 
-int main()
-{
-    clampedBits left = "100010101";
-    left = left + 1;
-    std::cout << left << '\n';
-    return 0;
+int main() {
+  clampedBits bits(1, 1);
+  for (uint64_t i = 0; i < 128; i++) {
+    bits <<= 2;
+    if (i % 4 == 0) {
+      std::cout << std::format("Bits:\t{}\n", bits.str());
+      std::cout << std::format("Value:\t{}\n", bits.convert_to_base(10));
+    }
+  }
+
+  return 0;
 }
